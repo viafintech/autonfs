2a5d1f3f9dcaa346d311438c5ee8bdc8ed6ee5cb
Martin Seener <martin.seener@barzahlen.de>
2013-08-22 18:00:57 +0200

    added initial autonfs.sh and init-script as well as an updated README with more documentation and install instructions

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-AutoNFS
-=======
+# AutoNFS
 
-AutoNFS is a client-side autofs-free NFS Share Automount-Script
+AutoNFS is a client-side autofs-free NFS Share Automount-Script, initially designed for Debian Squeeze or derivates.
+
+The initial Github version of AutoNFS v1.2.1 - as is - running quite well and stable in our production environment containing of about 20 NFS Shares and one NFS 3 Server - all with latest Debian Squeeze
+
+## Installation and Usage
+
+The locations of the files are given through the folders in this Repository, so for example the autonfs.sh should reside in /usr/local/bin folder in your NFS-Client
+
+1. Place a copy of the autonfs.sh (actual script) in the desired folder
+..- Modify the file by setting the correct `NFSSERVER` and `MOUNTS` vars and optionally `NFSVERS`, `MOUNTOPT` and `INTERVAL`
+2. Place a copy of the autonfs (LSB init script) file in the desired folder
+..- Make both executable with `chmod +x /<path to the files>`
+3. Install the init script for bootup with `update-rc.d autonfs defaults`
+4. Start AutoNFS with `service autonfs start` - the mounted folders will magically appear
+
+AutoNFS will now completly take care of those mounts by checking the NFS Server regularily (`INTERVAL`) if its up or down. If its down, it will unmount the shares after 2 check-failures and will auto-remount them as soon as the server is back up. In that case it will also write it to the syslog.
+
+You can also uncomment all lines starting with `#logger` to get a more verbose syslog output (not needed in production)
+
+## Variables explanation
+
+- `NFSSERVER` - The NFS Server you want to mount something from can be either a single IP or a DNS-Name
+- `NFSVERS` - Defines the NFS Protocol (2,3 or 4) to be used for checking the Server and is needed to prevent writing "unknown version" warnings in the syslog
+- `MOUNTOPT` - Here you can define the classic NFS Mount options like you would in the fstab (uses good defaults for most scenarios)
+- `INTERVAL` - How often should AutoNFS check the Server? (60seconds is a good default but we use 15seconds for our HA Environment)
+- `MOUNTS` - This is a space-separated list of shares to mount for ex. `MOUNTS=( "/share1" "/share2" )`, whereas the share name and mountpath must be the same in this version. In a later version i will also support different shares and mount-paths
+
+## Roadmap
+
+- [ ] Support for different share-names and mount-paths
+- [ ] A plain Bourne-shell compatible version of autonfs.sh (to be used with different shells than bash)
+- [ ] Make the init-script more flexible (different autonfs.sh locations for ex. by using a /etc/default/autonfs file)
+- [ ] ...(Ideas?)
+
+## Copyright and License
+
+This little tool was made by Martin Seener (c) 2012, 2013 with the initial idea from JeroenHoek at http://ubuntuforums.org/showthread.php?t=1389291 or https://github.com/jdhoek
+
+Released under the GNU GPLv2
---
+++ usr/local/bin/autonfs.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# AutoNFS v1.2.1
+
+# (c) 2012/2013 by Martin Seener (martin@seener.de)
+# Licensed under the GPLv2
+
+# Code can be found at github.com/martinseener/autonfs
+# Original Idea from JeroenHoek at http://ubuntuforums.org/showthread.php?t=1389291
+
+# Hints:
+#   - only limited logging is active; for debug logging, uncomment all "logger" lines
+
+# Changelog:
+#   v1.0:
+#     - initial version
+#   v1.1:
+#     - added NFSVERS parameter which prevents the server from writing
+#       "unkown version" warnings into syslog for each call
+#   v1.2 (2013-04-10):
+#     - improved NFS Server check (double checks its N/A before unmounting)
+#       - sometimes the first check is a false positive, therefore doing two
+#     - improved debug syslogging (added tag and more useful information)
+#     - added some more inline comments for explanation
+#   v1.2.1 (2013-04-12)
+#     - logging is only done when something is not working in first place (limited logging)
+
+# The hostname or IP-address of the fileserver:
+FILESERVER="nfsserver.example.com"
+# The NFS Version to be checked (must given to prevent "unknown version" syslog warnings)
+NFSVERS=3
+# Mount Options (see mount man pages for info)
+MOUNTOPT="-o rw,hard,intr,tcp,actimeo=3"
+# Check every X seconds (60 is a good default):
+INTERVAL=60
+# The shares that need to be mounted:
+MOUNTS=( "/mnt/share" )
+
+# Write PID to a PIDFILE for later process kill
+# The init script handles the kill process and if there`s already a process running
+echo $$ > /var/run/autonfs.pid
+
+while true; do
+  # First check if Fileserver is serving NFS
+  /usr/bin/rpcinfo -t "$FILESERVER" nfs $NFSVERS &>/dev/null
+  if [ $? -ne 0 ]; then
+    # First check failed, therefore doing it twice after a second
+    logger -t autonfs "Fileserver[${FILESERVER}] seems to be down - Rechecking in a second"
+    sleep 1
+    /usr/bin/rpcinfo -t "$FILESERVER" nfs $NFSVERS &>/dev/null
+    if [ $? -eq 0 ]; then
+      # Second check was succssful, checking if all NFS Shares are mounted
+      logger -t autonfs "Fileserver[${FILESERVER}] is up"
+      for MOUNT in ${MOUNTS[@]}; do
+        mount | grep -E "^${FILESERVER}:${MOUNT} on .*$" &>/dev/null
+        if [ $? -ne 0 ]; then
+          # The actual NFS Share is not mounted, so it will be done now
+          #logger -t autonfs "NFS-Share[${MOUNT}] not mounted - attempting to mount it from: ${FILESERVER}:${MOUNT}"
+          mount -t nfs ${MOUNTOPT} ${FILESERVER}:${MOUNT} ${MOUNT}
+        fi
+      done
+    else
+      # The Fileserver was not reachable two times, unmounting shares which are still mounted
+      logger -t autonfs "Fileserver(${FILESERVER}) is down."
+      for MOUNT in ${MOUNTS[@]}; do
+        mount | grep -E "^${FILESERVER}:${MOUNT} on .*$" &>/dev/null
+        if [ $? -eq 0 ]; then
+          logger -t autonfs "Cannot reach ${FILESERVER}, therefore unmounting NFS-Share[${MOUNT}]"
+          umount -f ${MOUNT}
+        fi
+      done
+    fi
+  else
+    # First check succeeded, so just checking if all NFS-Shares are mounted
+    #logger -t autonfs "Fileserver[${FILESERVER}] is up"
+    for MOUNT in ${MOUNTS[@]}; do
+      mount | grep -E "^${FILESERVER}:${MOUNT} on .*$" &>/dev/null
+      if [ $? -ne 0 ]; then
+        # The actual NFS Share is not mounted, so it will be done now
+        #logger -t autonfs "NFS-Share[${MOUNT}] not mounted - attempting to mount it from: ${FILESERVER}:${MOUNT}"
+        mount -t nfs ${MOUNTOPT} ${FILESERVER}:${MOUNT} ${MOUNT}
+      fi
+    done
+  fi
+  # Gone through the check iteration - sleeping now for a while
+  sleep $INTERVAL
+done
